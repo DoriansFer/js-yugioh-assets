190c822f683847a8d5a71c402105f5023a588089
DoriansFer <doohfernandes@yahoo.com>
2024-03-01 13:00:29 -0300

    cards

---
+++ src/scripts/engine.js
@@ -0,0 +1,56 @@
+const state = {
+    score: {
+        playerScore: 0,
+        computerScore: 0,
+        scoreBox: document.getElementById("score_points"),
+    },
+    cardSprites: {
+        avatar: document.getElementById("card-image"),
+        name: document.getElementById("card-name"),
+        type: document.getElementById("card-type"),
+    },
+    fieldCards: {
+        player: document.getElementById("player-field-card"),
+        player: document.getElementById("computer-field-card"),
+    },
+    actions: {
+        button: document.getElementById("next-duel"),
+    },
+};
+
+const cardData = [
+    {
+        id: 0,
+        name: "Blue Eyes White Dragon",
+        type: "Paper",
+        img: ".src/assets/icons/dragon.png",
+        WinOf: [1],
+        LoseOf: [2],
+    },
+    {
+        id: 1,
+        name: "Dark Magician",
+        type: "Rock",
+        img: ".src/assets/icons/magician.png",
+        WinOf: [2],
+        LoseOf: [0],
+    },
+    {
+        id: 2,
+        name: "Exodia",
+        type: "Scissors",
+        img: ".src/assets/icons/exodia.png",
+        WinOf: [0],
+        LoseOf: [1],
+    }
+]
+
+/*const players = {
+    player1: "player-cards",
+}*/
+
+function init() {
+
+}
+
+init();
